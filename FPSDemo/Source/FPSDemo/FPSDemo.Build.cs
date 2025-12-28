d17fa46bc5c1f14e9c5fb33acece2499593e41f8
deliama <107669890+deliama@users.noreply.github.com>
2025-12-28 10:41:21 +0800

    Initialize the project

---
+++ FPSDemo/Source/FPSDemo/FPSDemo.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FPSDemo : ModuleRules
+{
+	public FPSDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"FPSDemo",
+			"FPSDemo/Variant_Horror",
+			"FPSDemo/Variant_Horror/UI",
+			"FPSDemo/Variant_Shooter",
+			"FPSDemo/Variant_Shooter/AI",
+			"FPSDemo/Variant_Shooter/UI",
+			"FPSDemo/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
